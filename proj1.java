df01e0699fbb45dd24d5dff527787fce4bafba99
Raul Rodriguez <raulr.ftech@icloud.com>
2026-09-15 20:37:44 -0600

    Proj reqs and somewhat of a plan

--- proj1.java
+++ proj1.java
@@ -2,4 +2,54 @@ class proj1 {
     public static void main(String[] args) {
         
     }
+
+    // Topic of this Project
+    //      Creating a program that incorps user input into conditional logic
+    //      Specifically, creating a quiz or questionnaire on a topic of interest
+    //      The output a user receives from your program will differ based on responses
+    // Project Reqs
+    //      Input/Output
+    //          ask user at least 5 mcq's. Each question myst clearly display the available answer choices
+    //      Variables
+    //          create vars to store data thatll be needed later in the program
+    //      Strings
+    //          Use 1D arrays to store questions, answer choices and user responses
+    //      Conditionals
+    //          Use if, else if, ternaries
+    //      1D Arrays
+    //          use 1F arrays to store questions, answer choices, responses and correct answers
+    //      Loops
+    //          use at least one for or while loop
+    //      Methods
+    //          create and call >=2 methods
+    //      Score/Result
+    //          calaculate and display final result
+    //      Answer Feedback
+    //          May tell the user whether an individual answer was correct or provide other feedback
+
+    
+    // Checkpoint (worth 10%)
+    //      Brief Descriptions of focus and final res users get
+    //          Itll be an mcq test on primitive types and last will be a fill in
+    //          Total of 5 questions
+    //              1-2: poor performance, practice will be suggested
+    //              3-4: average performance, recommondation based on what answers were wrong
+    //              5:   outstanding job, keep the roll
+    //      List of quesitons and possible responses
+    //      List of vars thatll be used
+    //          string to capture input, Integer.parseInt() to verify the last question is a valid number, sentinel value for early exit 
+    //      Outline of condiitonal statements and consequences
+    //          if (Integer.parseInt(lastInput).equals(Integer.MAX_VALUE)) but this would req a try/catch since parseInt etc throw
+    //          determining standing such as what to recommend
 }
+
+
+
+// Code Submission (worth 60%)
+
+
+
+// Flowchart Submission (worth 10%)
+
+
+// Explanation (worth 20%)
